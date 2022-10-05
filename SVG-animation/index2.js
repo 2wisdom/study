279037aa29ac85190e49bec5d7b01fe52abdd02d
Lizzie <leejihye7117@gmail.com>
2022-10-06 02:22:32 +0900

    yesss

--- SVG-animation/index2.js
+++ SVG-animation/index2.js
@@ -1,14 +1,9 @@
-const content1 = document.querySelector(".content1");
-const path1 = document.querySelector(".path1");
-const path1Length = path1.getTotalLength();
+const content = document.querySelector(".content");
+const path = document.querySelector(".path");
+const pathLength = path.getTotalLength();
 
-path1.style.strokeDasharray = path1Length;
-path1.style.strokeDashoffset = path1Length;
-// calcDashoffset(
-//   window.innerHeight * 0.8,
-//   content1,
-//   path1Length
-// );
+path.style.strokeDasharray = pathLength;
+path.style.strokeDashoffset = pathLength;
 
 const calcDashoffset = (scrollY, element, length) => {
   const ratio = (scrollY - element.offsetTop) / element.offsetHeight;
@@ -18,7 +13,7 @@ const calcDashoffset = (scrollY, element, length) => {
 
 const scrollHandler = () => {
   const scrollY = window.scrollY + window.innerHeight * 0.8;
-  path1.style.strokeDashoffset = calcDashoffset(scrollY, content1, path1Length);
+  path.style.strokeDashoffset = calcDashoffset(scrollY, content, pathLength);
 };
 
 window.addEventListener("scroll", scrollHandler);
